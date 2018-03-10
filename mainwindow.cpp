6732d513ace1bd0f50b853852ccc402ff4712161
Igor Sosa Mayor <joseleopoldo1792@gmail.com>
2018-03-10 16:16:32 +0100

    Conectando a la db

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,8 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QDebug>
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
@@ -11,4 +13,9 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     delete ui;
+    db = QSqlDatabase::addDatabase("QSQLITE");
+    db.setDatabaseName("libros.db");
+
+    if (db.open())
+        qDebug() << "Abierta";
 }
